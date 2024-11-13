f0d86d0675cd93d9bbf5be873be0b5c8e3f879d9
pranav123-dev <pranavnandhu123456@gmail.com>
2024-11-13 09:51:59 +0530

    Add files via upload

---
+++ arraydelete.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+  int a[10],i,n,pos;
+  printf("Enter the limit:");
+  scanf("%d",&n);
+  printf("Enter the array elements:");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  printf("\n Given array is:");
+  for(i=0;i<n;i++)
+  {
+    printf("%d",a[i]);
+  }
+  printf("Enter the position to be deleted:");
+  scanf("%d",&pos);
+  for(i=pos;i<n;i++)
+    {
+       a[i]=a[i+1];
+    }
+   printf("\n Converted Array:");
+   for(i=0;i<n-1;i++)
+   {
+     printf("%d",a[i]);
+   } 
+  }        
